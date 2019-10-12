c2319d6c9506545cf0610cbf68ed3185dc87d4c9
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-10-12 11:37:12 +0800

    update fano

--- src/app.pas
+++ src/app.pas
@@ -14,15 +14,24 @@ uses
 
 var
     appInstance : IWebApplication;
+    cliParams : ICliParams;
+    host : string;
+    port : word;
 
 begin
-    writeln('Starting application at 127.0.0.1:4000');
+    cliParams := (TGetOptsParams.create() as ICliParamsFactory)
+        .addOption('host', 1)
+        .addOption('port', 1)
+        .build();
+    host := cliParams.getOption('host', '127.0.0.1');
+    port := cliParams.getOption('port', 4000);
+    writeln('Starting application at ', host, ':', port);
 
     (*!-----------------------------------------------
      * Bootstrap application
      *
      * @author AUTHOR_NAME <author@email.tld>
      *------------------------------------------------*)
-    appInstance := TBootstrapApp.create('127.0.0.1', 4000);
+    appInstance := TBootstrapApp.create(host, port);
     appInstance.run();
 end.
